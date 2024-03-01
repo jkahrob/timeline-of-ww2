a90aefb4b097f9e9d0757afefb8ee2a685ddeec3
jkahrob <junakuula900@outlook.com>
2024-03-01 21:53:33 +0200

    Responsivity added for mobile screens

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Timeline from './Timeline';
 import jsonData from './ww2events.json';
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
 
 const App = () => {
     return (
         <div className="app-container">
-            <Header />
+            <Header/>
             <h1>World War 2: Timeline of Events</h1>
             <Timeline events={jsonData} />
+            <Footer/>
         </div>
     );
 };
