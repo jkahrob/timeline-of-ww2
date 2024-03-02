507ed6364c2548fc3344964464f57950d818303e
jkahrob <junakuula900@outlook.com>
2024-03-02 02:32:00 +0200

    Revert "Updates"
    This reverts commit c86c3f424dceb5818230839c85a87b6f1deced03.

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="page-footer">
+            <p>&copy; Created using React</p>
+        </footer>
+    );
+};
+
+export default Footer;
